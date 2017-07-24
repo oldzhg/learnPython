19a5eceb2948240923528d0ab4348d0f4c28c047
oldzhg <zcy000321@gmail.com>
2017-07-24 16:09:55 +0800

    Learning python

---
+++ cj.py
@@ -0,0 +1,13 @@
+#-*-coding:utf-8-*-
+def move(n, a, b, c):
+    if n == 1:
+        print(a, '->', c)
+    else:
+        move(n-1, a, c, b)
+        move(1, a, b, c)
+        move(n-1, b, a, c)
+
+n = input("请输入要移动的圆盘数量:")
+move(int(n), 'A', 'B', 'C')
+        
+    
