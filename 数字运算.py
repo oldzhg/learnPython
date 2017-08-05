9d2ae5fd4dfc4d50cb039cf5bafdee6c485543da
oldzhg <zcy000321@gmail.com>
2017-08-05 20:56:04 +0800

    map and reduce

--- 数字运算.py
+++ 数字运算.py
@@ -5,11 +5,17 @@ d = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9'
 
 
 def str2float(s):
-    b = -1
-    a = 0
-    for i in s:
-        if i == '.':
-            a = 1
-        if a == 1:
-            b = b + 1
+    def str2int(S):
+        return reduce(lambda x, y:x * 10 + y, map(lambda x: d[x], s))
+    index = s.find('.')
+    if index == -1:
+        return str2int(s)
+    s = s.replace('.','')
+    return str2int(s)/ math.pow(10,len(s)-index)
+
+
+c = '123.456'
+print(str2float(c))
+
+
 
