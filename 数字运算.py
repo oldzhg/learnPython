ddf50cd9db837dd548e6cba54dc8156f388dedae
oldzhg <zcy000321@gmail.com>
2017-08-05 21:11:27 +0800

    习惯纠正

--- 数字运算.py
+++ 数字运算.py
@@ -5,17 +5,15 @@ d = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9'
 
 
 def str2float(s):
-    def str2int(S):
-        return reduce(lambda x, y:x * 10 + y, map(lambda x: d[x], s))
+    def str2int(s):
+        return reduce(lambda x, y: x * 10 + y, map(lambda x: d[x], s))
+
     index = s.find('.')
     if index == -1:
         return str2int(s)
-    s = s.replace('.','')
-    return str2int(s)/ math.pow(10,len(s)-index)
+    s = s.replace('.', '')
+    return str2int(s) / math.pow(10, len(s) - index)
 
 
 c = '123.456'
 print(str2float(c))
-
-
-
