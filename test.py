1d896f8923e36c86e47f3d94afc21231c1894ccf
oldzhg <zcy000321@gmail.com>
2017-08-05 21:48:56 +0800

    使用埃氏筛法寻找素数

--- test.py
+++ test.py
@@ -1,1 +1,25 @@
-#测试git中文情况
+def odd():
+    s = 1
+    while True:
+        s = s + 2
+        yield s
+
+
+def cj(m):
+    return lambda x: x % m > 0
+
+
+def primes():
+    yield 2
+    it = odd()
+    while True:
+        a = next(it)
+        yield a
+        it = filter(cj(a), it)
+
+
+for n in primes():
+    if n < 1000:
+        print(n)
+    else:
+        break
